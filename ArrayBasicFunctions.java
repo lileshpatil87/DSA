ecad9a3e83ba3090fc0ac9bc3c49ee8525445e92
Lilesh-Patil-Dev <lileshpatil38@gmail.com>
2026-04-28 09:07:50 +0530

    Day 6 Morning Session

---
+++ ArrayBasicFunctions.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+
+public class ArrayBasicFunctions {
+    public static void main(String args[]) {
+        int arr[] = { 1, 4, 2, 6, 3, 7 };
+        arrayReverse(arr);
+        System.out.println("Reverse of [1,4,2,6,3,7] : " + Arrays.toString(arr));
+        int pairs[][] = pairsInArray(arr);
+        System.out.println("Pairs in the array : ");
+        for (int i = 0; i < pairs.length; i++) {
+            System.out.print(Arrays.toString(pairs[i]) + " ");
+        }
+    }
+
+    // Array Reverse
+    // Time Complexity O(n) / O(n/2) => O(n)
+    public static void arrayReverse(int arr[]) {
+        int start = 0;
+        int end = arr.length - 1;
+        int temp = 0;
+        while (start < end) {
+            temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    // Pairs in Array
+    // Time Complexity O(n^2)
+    public static int[][] pairsInArray(int arr[]) {
+        int pairs[][] = new int[arr.length * (arr.length - 1) / 2][2];
+        int pairsCounter = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                pairs[pairsCounter++] = new int[] { arr[i], arr[j] };
+            }
+        }
+        return pairs;
+    }
+}
